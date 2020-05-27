d5aabb1ce5ba35488b5e6d6c315047a541810c94
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-05-27 21:40:19 +0900

    some mkdir

---
+++ shishaLog/shishaLog/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  shishaLog
+//
+//  Created by 宮本一成 on 2020/05/27.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import Firebase
+import FirebaseStorage
+
+
