4f45da07003ecc95eb731ffba10795394547fbe6
Isseymiyamoto <ants.can.walk@gmail.com>
2020-05-27 21:23:30 +0900

    inital commit

---
+++ shishaLog/shishaLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shishaLog
+//
+//  Created by 宮本一成 on 2020/05/27.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
