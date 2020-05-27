fde0fa9d55127ecf6918acb8e2c272269fad4935
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-05-27 21:55:40 +0900

    change first window

---
+++ shishaLog/shishaLog/Controllers/MainTabController.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabController.swift
+//  shishaLog
+//
+//  Created by 宮本一成 on 2020/05/27.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class MainTabController: UITabBarController{
+    
+    // MARK: - Properties
+    
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .twitterBlue
+        configureUI()
+    }
+    
+    
+    // MARK: - API
+    
+    
+    // MARK: - Helpers
+    
+    func configureUI(){
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
