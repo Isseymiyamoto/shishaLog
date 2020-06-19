053005028cabe49101a177351c916fa3b9b4197f
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-06-19 18:50:00 +0900

    configure collectionView on profileFilterView

--- shishaLog/shishaLog/View/Profile/ProfileFilterCell.swift
+++ shishaLog/shishaLog/View/Profile/ProfileFilterCell.swift
@@ -12,6 +12,10 @@ class ProfileFilterCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    var option: ProfileFilterOptions! {
+        didSet{ titleLabel.text = option.description }
+    }
+    
     let titleLabel: UILabel = {
         let label = UILabel()
         label.textColor = .lightGray
--- shishaLog/shishaLog/View/Profile/ProfileFilterView.swift
+++ shishaLog/shishaLog/View/Profile/ProfileFilterView.swift
@@ -43,6 +43,8 @@ class ProfileFilterView: UIView {
         
         collectionView.register(ProfileFilterCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         
+        let selectedIndexPath = IndexPath(row: 0, section: 0)
+        collectionView.selectItem(at: selectedIndexPath, animated: true, scrollPosition: .left)
         
         addSubview(collectionView)
         collectionView.addConstraintsToFillView(self)
@@ -51,31 +53,51 @@ class ProfileFilterView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    
-    
-    // MARK: - Selectors
-    
-    // MARK: - Helpers
-   
 }
+
+// MARK: - UICollectionViewDataSource
 
 extension ProfileFilterView: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return ProfileFilterOptions.allCases.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ProfileFilterCell
+        
+        let option = ProfileFilterOptions(rawValue: indexPath.row)
+        cell.option = option
+        
+        return cell
     }
-    
-    
 }
+
+// MARK: - UICollectionViewDelegate
 
 extension ProfileFilterView: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        let xPosition = cell?.frame.origin.x ?? 0
+        
+        UIView.animate(withDuration: 0.3) {
+            self.underlineView.frame.origin.x = xPosition
+        }
+        // どこが押されたかProfileController側に通知して、それに関連したデータをfetchする
+        delegate?.filterView(self, didSelect: indexPath.row)
+    }
     
 }
 
+// MARK: - UICollectionViewDelegateFlowLayout
+
 extension ProfileFilterView: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let count = CGFloat(ProfileFilterOptions.allCases.count)
+        return CGSize(width: frame.width / count, height: frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
     
 }
