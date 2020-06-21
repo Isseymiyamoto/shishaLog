c04d48bcc1a97de31123912ee8c416442e0f8412
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-06-22 01:12:44 +0900

    set the like button ui

--- shishaLog/shishaLog/View/LogCell.swift
+++ shishaLog/shishaLog/View/LogCell.swift
@@ -87,7 +87,8 @@ class LogCell: UICollectionViewCell {
         let button = UIButton(type: .system)
         button.setImage(UIImage(systemName: "star"), for: .normal)
         button.addTarget(self, action: #selector(handleLikeButtonTapped), for: .touchUpInside)
-        button.setDimensions(width: 24, height: 24)
+        button.setDimensions(width: 20, height: 20)
+        button.tintColor = .shishaColor
         button.imageView?.contentMode = .scaleAspectFit
         return button
     }()
