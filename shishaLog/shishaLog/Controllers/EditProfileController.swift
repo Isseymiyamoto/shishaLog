212345c13d1d89d2543083fcaddaed71dee0d0f4
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-06-24 15:19:30 +0900

    make editProfileController

---
+++ shishaLog/shishaLog/Controllers/EditProfileController.swift
@@ -0,0 +1,32 @@
+//
+//  EditProfileController.swift
+//  shishaLog
+//
+//  Created by 宮本一成 on 2020/06/24.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileController: UITableViewController {
+    
+    // MARK: - Properties
+
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    // MARK: - Helpers
+
+}
+
+// MARK: - UITableViewDataSource / Delegate
+
+extension EditProfileController{
+    
+}
+
