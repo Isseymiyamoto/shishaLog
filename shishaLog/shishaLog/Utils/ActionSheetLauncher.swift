98f03d1248c473414d534a957da4c8866a916207
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-07-25 20:48:10 +0900

    make actionSheetLauncher1

---
+++ shishaLog/shishaLog/Utils/ActionSheetLauncher.swift
@@ -0,0 +1,82 @@
+//
+//  ActionSheetLauncher.swift
+//  shishaLog
+//
+//  Created by 宮本一成 on 2020/07/25.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "ActionSheetCell"
+
+protocol ActionSheetLauncherDelegate: class {
+    func didSelect()
+}
+
+class ActionSheetLauncher: NSObject {
+    
+    // MARK: - Properties
+    
+    private let user: User
+    private let tableView = UITableView()
+    private var window: UIWindow?
+    // viewmodel作成
+    weak var delegate: ActionSheetLauncherDelegate?
+    private var tableViewHeight: CGFloat?
+    
+    private lazy var blackView: UIView = {
+        let view = UIView()
+        view.alpha = 0
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismissal))
+        view.addGestureRecognizer(tap)
+        return view
+    }()
+    
+    private lazy var footerView: UIView = {
+        let view = UIView()
+        
+        view.addSubview(cancelButton)
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.anchor(left: view.leftAnchor, right: view.rightAnchor,
+                            paddingLeft: 12, paddingRight: 12)
+        cancelButton.centerY(inView: view)
+        cancelButton.layer.cornerRadius = 50 / 2
+        return view
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("キャンセル", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemGroupedBackground
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    // MARK: - Lifecycle
+    
+    init(user: User) {
+        self.user = user
+        super.init()
+    }
+    
+    
+    // MARK: - Selectors
+    
+    @objc func handleDismissal(){
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+            self.tableView.frame.origin.y += 300
+        }
+    }
+    
+    
+    // MARK: - Helpers
+    
+    
+}
