9740907c0c0c8b555a48984957caeca9ef9ec841
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-07-29 14:04:31 +0900

    configure log controller 1

--- shishaLog/shishaLog/Controllers/SpotController.swift
+++ shishaLog/shishaLog/Controllers/SpotController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 private let reuseIdentifier = "SpotHeader"
+private let spotCellIdentifier = "SpotCell"
 
 class SpotController: UICollectionViewController {
     
@@ -42,7 +43,47 @@ class SpotController: UICollectionViewController {
     func configureCollecitonView(){
         collectionView.backgroundColor = .white
         
-//        collectionView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellWithReuseIdentifier: <#T##String#>)
+        collectionView.register(SpotHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: reuseIdentifier)
+    }
+}
+
+
+// MARK: UICollectionViewDataSource / Delegate
+
+extension SpotController{
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: reuseIdentifier, for: indexPath) as! SpotHeader
+        header.delegate = self
+        header.spot = spot
+        return header
+    }
+    
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+
+extension SpotController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 240)
+    }
+}
+
+
+// MARK: - SpotHeaderDelegate
+
+extension SpotController: SpotHeaderDelegate{
+    func handleProfileImageTapped(user: User) {
+        let controller = ProfileController(user: user)
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
+    func showActionSheet() {
+        print("DEBUG: show action sheet here...")
     }
     
     
