65112ffa467961dbc57a611e411b10eb489d5a39
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-08-04 14:57:31 +0900

    make shop contorller

---
+++ shishaLog/shishaLog/Controllers/ShopController.swift
@@ -0,0 +1,46 @@
+//
+//  ShopController.swift
+//  shishaLog
+//
+//  Created by 宮本一成 on 2020/08/04.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class ShopController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let shopID: String
+    
+    
+    // MARK: - Lifecycle
+    
+    init(shopID: String){
+        self.shopID = shopID
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+    
+    
+    // MARK: - Helpers
+    
+    
+    
+    
+    
+    
+    
+}
