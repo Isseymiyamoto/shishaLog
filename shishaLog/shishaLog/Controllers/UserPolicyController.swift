7c902d0874192e7f85a7cca9ac76a47028b09d27
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-08-20 19:40:40 +0900

    利用規約ようのcontroller作成

---
+++ shishaLog/shishaLog/Controllers/UserPolicyController.swift
@@ -0,0 +1,38 @@
+//
+//  UserPolicyController.swift
+//  shishaLog
+//
+//  Created by 宮本一成 on 2020/08/20.
+//  Copyright © 2020 ISSEY MIYAMOTO. All rights reserved.
+//
+
+import UIKit
+
+class UserPolicyController: UIViewController{
+    
+    // MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+ 
+    
+    
+    // MARK: - Helpers
+    
+    
+    
+    
+}
+
+
