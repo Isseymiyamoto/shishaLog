68182df5df4c76519b4428bcc4c7dc77710038a6
IsseiMiyamoto <ants.can.walk@gmail.com>
2020-08-22 21:12:21 +0900

    user policy controller , configureUI

--- shishaLog/shishaLog/Controllers/UserPolicyController.swift
+++ shishaLog/shishaLog/Controllers/UserPolicyController.swift
@@ -12,9 +12,34 @@ class UserPolicyController: UIViewController{
     
     // MARK: - Properties
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        return label
+    private let userPolicyTextView: UITextView = {
+        let tv = UITextView()
+        return tv
+    }()
+    
+    private let agreeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("同意する", for: .normal)
+        button.titleLabel?.textAlignment = .center
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 12)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .systemBlue
+        button.addTarget(self, action: #selector(handleAgreeUserPolicy), for: .touchUpInside)
+        button.layer.cornerRadius = 32 / 2
+        return button
+    }()
+    
+    private let cancelButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 12)
+        button.titleLabel?.textAlignment = .center
+        button.backgroundColor = .white
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.systemBlue.cgColor
+        button.layer.cornerRadius = 32 / 2
+        button.addTarget(self, action: #selector(handleCancel), for: .touchUpInside)
+        return button
     }()
     
     
@@ -23,13 +48,28 @@ class UserPolicyController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configure()
     }
     
  
+    // MARK: - Selectors
     
+    @objc func handleAgreeUserPolicy(){
+        // mainTabBarControllerに移動する
+    }
+    
+    @objc func handleCancel(){
+        self.dismiss(animated: true, completion: nil)
+    }
     
     // MARK: - Helpers
     
+    func configure(){
+        userPolicyTextView.text = ""
+        
+        view.addSubview(userPolicyTextView)
+        
+    }
     
     
     
